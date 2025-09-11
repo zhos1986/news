c3b9b79adc32bedb99caf0f19a941b7e4da5afb8
zhos1986 <42479536+zhos1986@users.noreply.github.com>
2025-09-11 21:16:04 +0500

    123

--- news/resources/views/welcome.blade.php
+++ news/resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +8,7 @@
 	<title>Document</title>
 </head>
 <body>
+<p class="text-3xl">143</p>
 
 </body>
 </html>
