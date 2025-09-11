f82ebc765b5c4b6cd4ea3a4d46a83622523739f8
Zhos Dmitrii <zhos@bk.ru>
2025-09-11 12:37:12 +0500

    11-09-2011 12:37

--- news/resources/views/welcome.blade.php
+++ news/resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	@vite('resources/css/app.css')
 	<title>Document</title>
 </head>
 <body>
-<p class="text-3xl">123</p>
+
 </body>
 </html>
