a9d9ee8664ddd0e62959afdf71130b4279f45192
Zhos Dmitrii <zhos@bk.ru>
2025-09-25 15:48:51 +0500

    25-09-2025_work

--- news/resources/views/welcome.blade.php
+++ news/resources/views/welcome.blade.php
@@ -9,13 +9,17 @@
 </head>
 <body>
 
-<header class="bg-white">
-	<nav aria-label="Global" class="flex items-center justify-between p-6 mx-auto max-w-7xl lg:px-8 place-self-center">
-	  <div class="flex lg:flex-1">
+	<div class="h-24">
 
-	  </div>
+			</div>
+
+
+<header class="bg-neutral-950">
+
+	<nav aria-label="Global" class="flex items-center justify-between p-2 mx-auto max-w-7xl lg:px-8 place-self-center">
+
 	  <div class="flex lg:hidden">
-		<button type="button" command="show-modal" commandfor="mobile-menu" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+		<button type="button" command="show-modal" commandfor="mobile-menu" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-neutral-100">
 		  <span class="sr-only">Open main menu</span>
 		  <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
 			<path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -23,20 +27,24 @@
 		</button>
 	  </div>
 	  <el-popover-group class="hidden lg:flex lg:gap-x-12">
-		<a href="#" class="font-semibold text-gray-900 text-sm/6">Product</a>
-		<a href="#" class="font-semibold text-gray-900 text-sm/6">Features</a>
-		<a href="#" class="font-semibold text-gray-900 text-sm/6">Marketplace</a>
-		<a href="#" class="font-semibold text-gray-900 text-sm/6">Company</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">Общество</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">Экономика</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">Культура</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">Наши люди</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">Происшествия</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">ЖКХ</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">Производство</a>
+		<a href="#" class="font-semibold text-neutral-200 text-sm/6">Спорт</a>
 	  </el-popover-group>
 
 	</nav>
 	<el-dialog>
 	  <dialog id="mobile-menu" class="backdrop:bg-transparent lg:hidden">
 		<div tabindex="0" class="fixed inset-0 focus:outline-none">
-		  <el-dialog-panel class="fixed inset-y-0 right-0 z-50 w-full p-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+		  <el-dialog-panel class="fixed inset-y-0 right-0 z-50 w-full p-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-neutral-900/10">
 			<div class="flex items-center justify-between">
 
-			  <button type="button" command="close" commandfor="mobile-menu" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+			  <button type="button" command="close" commandfor="mobile-menu" class="-m-2.5 rounded-md p-2.5 text-neutral-700">
 				<span class="sr-only">Close menu</span>
 				<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
 				  <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
@@ -44,12 +52,16 @@
 			  </button>
 			</div>
 			<div class="flow-root mt-6">
-			  <div class="-my-6 divide-y divide-gray-500/10">
+			  <div class="-my-6 divide-y divide-neutral-500/10">
 				<div class="py-6 space-y-2">
-				  <a href="#" class="block px-3 py-2 -mx-3 font-semibold text-gray-900 rounded-lg text-base/7 hover:bg-gray-50">Product</a>
-				  <a href="#" class="block px-3 py-2 -mx-3 font-semibold text-gray-900 rounded-lg text-base/7 hover:bg-gray-50">Features</a>
-				  <a href="#" class="block px-3 py-2 -mx-3 font-semibold text-gray-900 rounded-lg text-base/7 hover:bg-gray-50">Marketplace</a>
-				  <a href="#" class="block px-3 py-2 -mx-3 font-semibold text-gray-900 rounded-lg text-base/7 hover:bg-gray-50">Company</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">Общество</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">Экономика</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">Культура</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">Наши люди</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">Происшествия</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">ЖКХ</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">Производство</a>
+				  <a href="#" class="block px-5 py-2 -mx-6 font-semibold text-neutral-900 text-base/7 hover:bg-neutral-100">Спорт</a>
 				</div>
 
 			  </div>
