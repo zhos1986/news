bc08946ab1db746db43be7ef6239c227e50a383a
Zhos Dmitrii <zhos@bk.ru>
2025-11-07 12:56:56 +0500

    07-11-2025_work

--- news/resources/views/welcome.blade.php
+++ news/resources/views/welcome.blade.php
@@ -87,33 +87,8 @@
 </header>
 
 
-<main class="grid grid-cols-2 gap-8 px-2 mx-auto mt-10 max-w-7xl md:px-4">
-{{--
-<section class="bg-neutral-100">
-	<div>
 
-		<h7 class="uppercase font-medium font-[Fira_Sans] tracking-widest text-neutral-600 text-sm">главные новости</h7>
-
-	<hr class="h-px my-4 bg-gray-600">
-	</div>
-</section>
-
-<section class="w-60 bg-neutral-100">
-
-<div>
-
-	<h7 class=" uppercase font-medium font-[Fira_Sans] tracking-widest text-neutral-600 text-sm">последние новости</h7>
-
-<hr class="h-px my-4 bg-gray-600">
-</div>
-
-</section> --}}
-
-
-
-</main>
-
-<div class="grid grid-cols-1 gap-8 md:grid-cols-[1fr_1fr_240px] md:grid-rows-7 px-2 mx-auto mt-10 max-w-7xl md:px-4">
+<main class="grid grid-cols-1 gap-8 md:grid-cols-[1fr_1fr_240px] md:grid-rows-7 px-2 mx-auto mt-10 max-w-7xl md:px-4">
 	<!-- 1 - на мобильных: первая строка, на десктопе: grid-row: 1 / 2; grid-column: 1 / 3; -->
 	<div class="bg-blue-200 md:row-start-1 md:row-end-2 md:col-start-1 md:col-end-3">
 
@@ -125,17 +100,55 @@
 	</div>
 
 	<!-- 2 - на мобильных: вторая строка, на десктопе: grid-row: 1 / 3; grid-column: 3 / 4; -->
-	<div class="bg-green-200 md:row-start-1 md:row-end-3 md:col-start-3 md:col-end-4 md:w-60">
+	<div class="flex flex-col md:row-start-1 md:row-span-full md:col-start-3 md:col-end-4 md:w-60">
 
-		<h7 class="uppercase font-medium font-[Fira_Sans] tracking-widest text-neutral-600 text-sm">главные новости</h7>
+		<div>
 
-		<hr class="h-px my-4 bg-gray-600">
+		<h7 class="uppercase font-medium font-[Fira_Sans] tracking-widest text-neutral-400 text-sm">последние новости</h7>
 
-	  Элемент 2
+		<hr class="my-4 border-t-2 border-gray-400">
 	</div>
 
+	  <div class="">
+
+		<time class="block mt-4 font-regular font-[Fira_Sans] tracking-widest text-neutral-400 text-xs" datetime="2025-11-07T14:54">2025-11-07 8:21</time>
+
+		<h3 class="font-regular font-[Fira_Sans] text-neutral-900 text-base">
+			Развитие профессионального образования обсудили в уральской столице
+		</h3>
+
+		<hr class="mt-2 border-gray-200 border-t-1">
+
+	  </div>
+
+	  <div class="">
+
+		<time class="block mt-4 font-regular font-[Fira_Sans] tracking-widest text-neutral-400 text-xs" datetime="2025-11-07T14:54">2025-11-07 8:21</time>
+
+		<h3 class="font-regular font-[Fira_Sans] text-neutral-900 text-base">
+			Развитие профессионального образования обсудили в уральской столице
+		</h3>
+
+		<hr class="mt-2 border-gray-200 border-t-1">
+
+	  </div>
+
+	  <div class="">
+
+		<time class="block mt-4 font-regular font-[Fira_Sans] tracking-widest text-neutral-400 text-xs" datetime="2025-11-07T14:54">2025-11-07 8:21</time>
+
+		<h3 class="font-regular font-[Fira_Sans] text-neutral-900 text-base">
+			Развитие профессионального образования обсудили в уральской столице
+		</h3>
+
+		<hr class="mt-2 border-gray-200 border-t-1">
+
+	  </div>
+
+
+
 	<!-- 3 - скрыт на мобильных, виден на десктопе: grid-row: 3 / 4; grid-column: 3 / 4; -->
-	<div class="hidden bg-yellow-200 md:block md:row-start-3 md:row-end-4 md:col-start-3 md:col-end-4 md:w-60">
+	<div class="hidden mt-8 bg-yellow-200 md:block">
 
 		<h7 class="uppercase font-medium font-[Fira_Sans] tracking-widest text-neutral-600 text-sm">новости партнёров</h7>
 
@@ -146,7 +159,7 @@
 
 
 	<!-- 3 - скрыт на мобильных, виден на десктопе: grid-row: 3 / 4; grid-column: 3 / 4; -->
-	<div class="hidden bg-yellow-200 md:block md:row-start-4 md:row-end-5 md:col-start-3 md:col-end-4 md:w-60">
+	<div class="hidden mt-8 bg-yellow-200 md:block">
 
 		<h7 class="uppercase font-medium font-[Fira_Sans] tracking-widest text-neutral-600 text-sm">голосование</h7>
 
@@ -154,6 +167,12 @@
 
 	  Элемент !!!
 	</div>
+
+
+
+
+	</div>
+
 
 	<!-- 4 - на мобильных: третья строка, на десктопе: grid-row: 2 / 3; grid-column: 1 / 3; -->
 	<div class="bg-red-200 md:row-start-2 md:row-end-3 md:col-start-1 md:col-end-3">
@@ -194,7 +213,7 @@
 
 	  Элемент 7
 	</div>
-  </div>
+  </main>
 
 
 </body>
